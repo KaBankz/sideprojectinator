eddeb6b62739ee23fafa70b6248fe36eff4f6b13
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-04-02 19:05:32 -0400

    Clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,47 +9,45 @@ export default async function Home() {
   const session = await getServerAuthSession();
 
   return (
-    <>
-      <main className="flex min-h-screen flex-col items-center justify-center">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <div className="flex flex-col items-center justify-center">
-            <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-              SideProject<span className="text-red-600">Inator</span>
-            </h1>
-            <h2 className="text-4xl font-semibold italic">
-              Where side projects are born
-            </h2>
-          </div>
-
-          <div className="group relative inline-flex">
-            <div className="animate-pulse absolute -inset-2 rounded-xl bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] bg-top blur-xl transition-all duration-1000 group-hover:-inset-3"></div>
-            <button className="relative inline-flex items-center justify-center rounded-xl bg-gray-900 px-8 py-4 text-lg font-bold text-white transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-gray-900 focus:ring-offset-2 group-hover:scale-105">
-              I&apos;m Feeling Lucky!
-            </button>
-          </div>
-
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl">
-              {hello ? hello.greeting : "Loading tRPC query..."}
-            </p>
-
-            <div className="flex flex-col items-center justify-center gap-4">
-              <p className="text-center text-2xl">
-                {session && <span>Logged in as {session.user?.name}</span>}
-              </p>
-              <Link
-                href={session ? "/api/auth/signout" : "/api/auth/signin"}
-                className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-              >
-                {session ? "Sign out" : "Sign in"}
-              </Link>
-            </div>
-          </div>
-
-          <CrudShowcase />
+    <main className="flex min-h-[calc(100vh-3.5rem)] flex-col items-center justify-center">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+            SideProject<span className="text-red-600">Inator</span>
+          </h1>
+          <h2 className="text-4xl font-semibold italic">
+            ˗ˏˋ Where side projects are born ˎˊ˗
+          </h2>
         </div>
-      </main>
-    </>
+
+        <div className="group relative inline-flex">
+          <div className="absolute -inset-2 animate-pulse rounded-xl bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] bg-top blur-xl transition-all duration-1000 group-hover:-inset-3"></div>
+          <button className="relative inline-flex items-center justify-center rounded-xl bg-gray-900 px-8 py-4 text-lg font-bold text-white transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-gray-900 focus:ring-offset-2 group-hover:scale-105">
+            I&apos;m Feeling Lucky!
+          </button>
+        </div>
+
+        <div className="flex flex-col items-center gap-2">
+          <p className="text-2xl">
+            {hello ? hello.greeting : "Loading tRPC query..."}
+          </p>
+
+          <div className="flex flex-col items-center justify-center gap-4">
+            <p className="text-center text-2xl">
+              {session && <span>Logged in as {session.user?.name}</span>}
+            </p>
+            <Link
+              href={session ? "/api/auth/signout" : "/api/auth/signin"}
+              className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+            >
+              {session ? "Sign out" : "Sign in"}
+            </Link>
+          </div>
+        </div>
+
+        <CrudShowcase />
+      </div>
+    </main>
   );
 }
 
