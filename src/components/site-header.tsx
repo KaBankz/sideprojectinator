858e06d161a94385df82c896a8d029f001f9f247
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-04-02 19:05:25 -0400

    Cleanup header profile

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -6,7 +6,7 @@ import { MainNav } from "~/components/main-nav";
 // import { MobileNav } from "~/components/mobile-nav";
 import { ModeToggle } from "~/components/mode-toggle";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
-import { Button, buttonVariants } from "~/components/ui/button";
+import { buttonVariants } from "~/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -23,7 +23,7 @@ export async function SiteHeader() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center">
+      <div className="container flex h-14 max-w-screen-xl items-center">
         <MainNav />
         {/* <MobileNav /> */}
         <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
@@ -67,39 +67,36 @@ export async function SiteHeader() {
 
             {session ? (
               <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant="ghost" className="w-9 px-0">
-                    <Avatar>
-                      <AvatarImage src={session.user?.image ?? ""} />
-                      <AvatarFallback>{session.user?.name}</AvatarFallback>
-                    </Avatar>
-                    <span className="sr-only">Profile</span>
-                  </Button>
+                <DropdownMenuTrigger asChild className="ml-4">
+                  <Avatar className="h-8 w-8 cursor-pointer outline outline-2 outline-slate-600">
+                    <AvatarImage src={session.user?.image ?? ""} />
+                    <AvatarFallback>{session.user?.name}</AvatarFallback>
+                  </Avatar>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="end" className="w-56">
-                  <DropdownMenuItem>
-                    <User className="mr-2 h-4 w-4" />
-                    <span>Profile</span>
-                    <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                  <DropdownMenuItem asChild>
+                    <Link href="/profile">
+                      <User className="mr-2 h-4 w-4" />
+                      <span>Profile</span>
+                      <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                    </Link>
                   </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <LogOut className="mr-2 h-4 w-4" />
-                    <span>Log out</span>
-                    <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+
+                  <DropdownMenuItem asChild>
+                    <Link href="/api/auth/signout">
+                      <LogOut className="mr-2 h-4 w-4" />
+                      <span>Log out</span>
+                      <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+                    </Link>
                   </DropdownMenuItem>
                 </DropdownMenuContent>
               </DropdownMenu>
             ) : (
               <Link
+                className={buttonVariants({ variant: "outline" })}
                 href="/api/auth/signin"
-                className={cn(
-                  buttonVariants({
-                    variant: "ghost",
-                  }),
-                  "w-9 px-0",
-                )}
               >
-                Sign in
+                Log In
               </Link>
             )}
           </nav>
