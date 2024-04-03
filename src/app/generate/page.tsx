52ca0e87ae0bb469c6e6519f19a824705a6c7292
KaBankz <20971850+KaBankz@users.noreply.github.com>
2024-04-03 14:11:07 -0400

    Temporary generate page

---
+++ src/app/generate/page.tsx
@@ -0,0 +1,7 @@
+export default async function GeneratePage() {
+  return (
+    <main className="flex min-h-[calc(100vh-3.5rem)] flex-col items-center justify-center">
+      <h1 className="text-6xl font-bold">Thinking...</h1>
+    </main>
+  );
+}
